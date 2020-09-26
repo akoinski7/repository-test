0e5009b232fc3e0c7ae770665ebf3689385b9cf1
akoinski7 <akoinski7@yahoo.com>
2020-09-26 14:29:19 -0400

    Added a new branch with new text

--- Test.java
+++ Test.java
@@ -2,6 +2,6 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello Github!");
-		System.out.println("Here's a new line!");
+		System.out.println("This is created in a separate branch! Hopefully to be merged to master soon!");
 	}
 }
